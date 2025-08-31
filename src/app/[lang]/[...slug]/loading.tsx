b2c738495ae59597574641cdfd92eaa3199b70b0
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2025-08-31 20:02:16 +0200

    edit loading

--- src/app/[lang]/[...slug]/loading.tsx
+++ src/app/[lang]/[...slug]/loading.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 export default function Loading() {
   return (
     <div className="loader-wrapper">
-      <span className="loader"></span>
+      <span className="loader">
+        <Image
+          src="https://cdn.sanity.io/images/x6jc462y/production/f070d11f862c00400711d6efca18504713a95c27-772x242.png"
+          alt="Loading..."
+          width={772}
+          height={242}
+        />
+      </span>
     </div>
   );
 }

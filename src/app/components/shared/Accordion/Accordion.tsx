8067fbe1cfc92ddc3f3968d052eb0c51d1f9acc2
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2026-01-28 17:50:38 +0100

    edit accordion faq

--- src/app/components/shared/Accordion/Accordion.tsx
+++ src/app/components/shared/Accordion/Accordion.tsx
@@ -2,38 +2,11 @@ import React, { ReactNode } from "react";
 import { Accordion as BaseAccordion } from "@szhsin/react-accordion";
 import styles from "./Accordion.module.scss";
 
-type AccordionProps = {
-  children: ReactNode;
-};
+type AccordionProps = { children: ReactNode };
 
 const Accordion: React.FC<AccordionProps> = ({ children }) => {
   return (
     <div className={styles.accordion}>
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "FAQPage",
-            mainEntity: React.Children.map(children, (child: any) => ({
-              "@type": "Question",
-              name: child.props.title,
-              acceptedAnswer: {
-                "@type": "Answer",
-                text: child.props.content
-                  ? child.props.content
-                      .map((block: any) =>
-                        block.children
-                          ? block.children.map((c: any) => c.text).join("")
-                          : ""
-                      )
-                      .join("")
-                  : "",
-              },
-            })),
-          }),
-        }}
-      />
       <BaseAccordion transition transitionTimeout={250} allowMultiple={false}>
         {children}
       </BaseAccordion>
